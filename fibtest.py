21db4d48de7bfa3b92643c9e89bae54d1044a4b1
vky <rigaut.theotime@gmail.com>
2022-01-23 12:30:42 +0100

    read inputs from env variables

--- fibtest.py
+++ fibtest.py
@@ -1,7 +1,9 @@
+import os
+
 # testing Fibonacci number function
 def fib(n: int) -> int:
     return n if n < 2 else fib(n-1)+fib(n-2)
 
 
 def test_fibonacci():
-    assert fib(10) == 54
+    assert fib(int(os.environ["FIB_INPUT"])) == 55
