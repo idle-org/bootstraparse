c061fb9215cdb95d8e61e5cb83b6c4db1286536b
vky <rigaut.theotime@gmail.com>
2022-01-24 22:09:01 +0100

    started work

--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,24 @@
 # Interprets config files
+
+import yaml
+class ConfigLoader():
+    """
+    Reads config files.
+    """
+
+    def __init__(self, configfile): 
+        with open(configfile, "r") as f:
+            self.loadedconf = yaml.safe_load(f)
+    def __getitem__(self, item):
+        return self.loadedconf[item]
+
+class UserConfig(ConfigLoader):
+    def __init__(self, configfile):
+        super().__init__(configfile)
+
+class GlobalConfig(ConfigLoader):
+    def __init__(self, configfile):
+        super().__init__(configfile)
+        
+
+conf = ConfigLoader("./example_userfiles/config/aliases.yml")
