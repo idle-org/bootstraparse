3cd6c5ed12c3a4ab4a8077249d925742e0d53021
vky <rigaut.theotime@gmail.com>
2022-01-30 00:00:56 +0100

    created logging initializer

--- src/bootstraparse/modules/error_mngr.py
+++ src/bootstraparse/modules/error_mngr.py
@@ -1,1 +1,20 @@
 # Dedicated module for defining and interpreting errors as well as generating error messages
+
+import logging
+
+
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+
+def initlogging(filename=None, loglevel="ERROR"):
+    """
+    Initializes logging
+    """
+    loglevel = loglevel.upper()
+    if loglevel not in ["ERROR", "INFO", "WARNING", "DEBUG", "CRITICAL"]:
+        print("Incorrect logging", loglevel + ". Defaulting to ERROR.")
+        loglevel = "ERROR"
+    logging.basicConfig(encoding="utf-8")
+    if filename is not None:
+        logging.basicConfig(filename=filename)
+    logging.basicConfig(level=logging.__getattribute__(loglevel))
