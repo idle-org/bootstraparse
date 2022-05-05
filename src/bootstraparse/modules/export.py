3867cb3d34a7f4753de8d9675a40d142637e85e2
vicky <rigaut.theotime@gmail.com>
2022-05-05 19:43:32 +0200

    testing relation to other modules

--- src/bootstraparse/modules/export.py
+++ src/bootstraparse/modules/export.py
@@ -1,1 +1,9 @@
 # Module for final generation of the translated website
+import rich
+from bootstraparse.modules import config, pathresolver
+
+
+all_configs = config.ConfigLoader(pathresolver.b_path("configs/"))
+all_templates = config.ConfigLoader(pathresolver.b_path("templates/"))
+rich.inspect(all_configs)
+rich.inspect(all_templates)
