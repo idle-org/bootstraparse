9d0c9af6f41a3aa90439f204488a43ccca2730e0
Maximilien Rigaut <max.rigaut@gmail.com>
2022-05-18 07:21:29 +0200

    Reverted changes, this .map needs a manual

--- src/bootstraparse/modules/context_mngr.py
+++ src/bootstraparse/modules/context_mngr.py
@@ -46,7 +46,7 @@ class BaseContainer:
         return self.content[start:end]
 
     def __rshift__(self, other):
-        return self.map[other]
+        return self.map[other]()
 
     def __eq__(self, other):
         if len(self) != len(other):
@@ -105,6 +105,20 @@ class BaseContainerWithOptionals(BaseContainer):
             if o.label == 'optional:class':
                 rlist += [o]
         return rlist
+
+    # def all_html_insert(self):
+    #     """
+    #     Returns a list of all html_inserts, and an empty list if none exist.
+    #     :rtype: list
+    #     """
+    #     return [o.content for o in self.fetch_html_insert()]
+    #
+    # def all_class_insert(self):
+    #     """
+    #     Returns a list of all class_inserts, and an empty list if none exist.
+    #     :rtype: list
+    #     """
+    #     return [o.content for o in self.fetch_class_insert()]
 
 
 # Define containers all the Enhanced text elements, divs, headers, list and any element that can be a container
