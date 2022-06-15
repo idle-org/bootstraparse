0f55dcd523c75895da296f3dcf349306c264d7d5
Maximilien Rigaut <max.rigaut@orange.fr>
2022-06-15 13:15:03 +0200

    Hotfix on the syntax with ulits

--- tests/modules/test_syntax.py
+++ tests/modules/test_syntax.py
@@ -524,6 +524,7 @@ dict_advanced_syntax_input_and_expected_output = {
         ), __GL()),
         ("|---|---|", (sy.TableSeparatorToken(["---", "---"]),), __GL()),
         ("- Text", (sy.EtUlistToken([sy.TextToken(["Text"])]),), __GL()),
+        (" - Non ulist", (sy.TextToken(["- Non ulist"]),), __GL()),
         ("div>>", (sy.StructuralElementEndToken(["div"]),), __GL()),
         ("#. Text with *em* and __underline__ and {arguments}{{super-arguments}}[a=1, c=2,'ui']", (
             sy.EtOlistToken([
@@ -666,6 +667,7 @@ list_of_text_input_and_readable_output = [
 
     # Other Mostly for testing and coverage
     ("'", "quotes", "'"),
+    (" -", "line", "-"),
 ]
 
 
